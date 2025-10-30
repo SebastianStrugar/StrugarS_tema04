841826b7dd0a193b1a92446605954521386c18b3
SebastianStrugar <sebastian.strugar@student.usv.ro>
2025-10-30 21:55:39 +0200

    Add project files.

---
+++ Camera3DIsometric.cs
@@ -0,0 +1,113 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+
+namespace ConsoleApp3
+{
+    class Camera3DIsometric
+    {
+        private Vector3 eye;
+        private Vector3 target;
+        private Vector3 up_vector;
+
+        private const int MOVEMENT_UNIT = 1;
+        private const float ROTATION_ANGLE = 0.05f; // radiani (~3°)
+
+        public Camera3DIsometric()
+        {
+            eye = new Vector3(200, 175, 25);
+            target = new Vector3(0, 25, 0);
+            up_vector = new Vector3(0, 1, 0);
+        }
+
+        public Camera3DIsometric(int _eyeX, int _eyeY, int _eyeZ, int _targetX, int _targetY, int _targetZ, int _upX, int _upY, int _upZ)
+        {
+            eye = new Vector3(_eyeX, _eyeY, _eyeZ);
+            target = new Vector3(_targetX, _targetY, _targetZ);
+            up_vector = new Vector3(_upX, _upY, _upZ);
+        }
+
+        public Camera3DIsometric(Vector3 _eye, Vector3 _target, Vector3 _up)
+        {
+            eye = _eye;
+            target = _target;
+            up_vector = _up;
+        }
+
+        public void SetCamera()
+        {
+            Matrix4 camera = Matrix4.LookAt(eye, target, up_vector);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref camera);
+        }
+
+        private void RotateAroundTarget(float angle)
+        {
+            Vector3 direction = eye - target;
+
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+
+            float x = direction.X * cos - direction.Z * sin;
+            float z = direction.X * sin + direction.Z * cos;
+
+            direction.X = x;
+            direction.Z = z;
+
+            eye = target + direction;
+            SetCamera();
+        }
+
+        public void RotateLeft()
+        {
+            RotateAroundTarget(ROTATION_ANGLE);
+        }
+
+        public void RotateRight()
+        {
+            RotateAroundTarget(-ROTATION_ANGLE);
+        }
+
+        public void MoveRight()
+        {
+            eye.Z -= MOVEMENT_UNIT;
+            target.Z -= MOVEMENT_UNIT;
+            SetCamera();
+        }
+
+        public void MoveLeft()
+        {
+            eye.Z += MOVEMENT_UNIT;
+            target.Z += MOVEMENT_UNIT;
+            SetCamera();
+        }
+
+        public void MoveForward()
+        {
+            eye.X -= MOVEMENT_UNIT;
+            target.X -= MOVEMENT_UNIT;
+            SetCamera();
+        }
+
+        public void MoveBackward()
+        {
+            eye.X += MOVEMENT_UNIT;
+            target.X += MOVEMENT_UNIT;
+            SetCamera();
+        }
+
+        public void MoveUp()
+        {
+            eye.Y += MOVEMENT_UNIT;
+            target.Y += MOVEMENT_UNIT;
+            SetCamera();
+        }
+
+        public void MoveDown()
+        {
+            eye.Y -= MOVEMENT_UNIT;
+            target.Y -= MOVEMENT_UNIT;
+            SetCamera();
+        }
+    }
+}
---
+++ Window3D.cs
@@ -0,0 +1,381 @@
+﻿using ConsoleApp3;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ConsoleApp3
+{
+    /// <summary>
+    /// The graphic window. Contains the canvas (viewport to be draw).
+    /// </summary>
+    class Window3D : GameWindow
+    {
+
+        private KeyboardState previousKeyboard;
+        private MouseState previousMouse;
+        private readonly Randomizer rando;
+        private readonly Axes ax;
+        private readonly Grid grid;
+        private readonly Camera3DIsometric cam;
+        private bool displayMarker;
+        private ulong updatesCounter;
+        private ulong framesCounter;
+        private MassiveObject objy;
+
+        private List<Cube> cubes = new List<Cube>();
+        private Random random = new Random();
+        private double spawnCooldown = 0;
+
+        private void SpawnCube()
+        {
+            var newCube = new Cube(
+                position: new Vector3(
+                    (float)(random.NextDouble() * 50),                // X între 0 și 50
+                    18.0f,                                            // Y 18
+                    (float)(random.NextDouble() * 50)                 // Z între 0 și 50
+                ),
+                size: (float)(random.NextDouble() * 2 + 3),           // Dimensiune între 3 și 5
+                color: new Color4(
+                    (float)random.NextDouble(),                       // R
+                    (float)random.NextDouble(),                       // G
+                    (float)random.NextDouble(),                       // B
+                    1.0f
+                )
+            );
+
+            cubes.Add(newCube);
+        }
+
+
+        // DEFAULTS
+        private readonly Color DEFAULT_BKG_COLOR = Color.FromArgb(49, 50, 51);
+
+        /// <summary>
+        /// Parametrised constructor. Invokes the anti-aliasing engine. All inits are placed here, for convenience.
+        /// </summary>
+        public Window3D() : base(1280, 768, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+
+
+            // inits
+            rando = new Randomizer();
+            ax = new Axes();
+            grid = new Grid();
+            cam = new Camera3DIsometric();
+            objy = new MassiveObject(Color.Yellow);
+
+            DisplayHelp();
+            displayMarker = false;
+            updatesCounter = 0;
+            framesCounter = 0;
+        }
+
+        /// <summary>
+        /// OnLoad() method. Part of the control loop of the OpenTK API. Executed only once.
+        /// </summary>
+        /// <param name="e">event parameters that triggered the method;</param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+        }
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if (e.Button == MouseButton.Left)
+            {
+                SpawnCube();
+            }
+            else if (e.Button == MouseButton.Right)
+            {
+                ClearCubes();
+            }
+
+        }
+        private void ClearCubes()
+        {
+            cubes.Clear();
+            Console.WriteLine("Lista de cuburi a fost curatata.");
+        }
+
+
+        /// <summary>
+        /// OnResize() method. Part of the control loop of the OpenTK API. Executed at least once (after OnLoad()).
+        /// </summary>
+        /// <param name="e">event parameters that triggered the method;</param>
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            // set background
+            GL.ClearColor(DEFAULT_BKG_COLOR);
+
+            // set viewport
+            GL.Viewport(0, 0, this.Width, this.Height);
+
+            // set perspective
+            Matrix4 perspectiva = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)this.Width / (float)this.Height, 1, 1024);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiva);
+
+            // set the eye
+            cam.SetCamera();
+        }
+
+        /// <summary>
+        /// OnUpdateFrame() method. Part of the control loop of the OpenTK API. Executed periodically, with a frequency determined when launching
+        /// the graphics window (<see cref="GameWindow.Run(double, double)"/>). In this case should be 30.00 (if unmodified).
+        ///
+        /// All logic should reside here!
+        /// </summary>
+        /// <param name="e">event parameters that triggered the method;</param>
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            updatesCounter++;
+
+            spawnCooldown -= e.Time;
+            spawnCooldown = Math.Max(spawnCooldown, 0);
+
+
+            if (displayMarker)
+            {
+                TimeStampIt("update", updatesCounter.ToString());
+            }
+
+            // LOGIC CODE
+            KeyboardState currentKeyboard = Keyboard.GetState();
+            MouseState currentMouse = Mouse.GetState();
+
+            if (currentKeyboard[Key.Escape])
+            {
+                Exit();
+            }
+
+            if (currentKeyboard[Key.H] && !previousKeyboard[Key.H])
+            {
+                DisplayHelp();
+            }
+
+            if (currentKeyboard[Key.R] && !previousKeyboard[Key.R])
+            {
+                GL.ClearColor(DEFAULT_BKG_COLOR);
+                ax.Show();
+                grid.Show();
+            }
+
+            if (currentKeyboard[Key.K] && !previousKeyboard[Key.K])
+            {
+                ax.ToggleVisibility();
+            }
+
+            if (currentKeyboard[Key.B] && !previousKeyboard[Key.B])
+            {
+                GL.ClearColor(rando.RandomColor());
+            }
+
+            if (currentKeyboard[Key.V] && !previousKeyboard[Key.V])
+            {
+                grid.ToggleVisibility();
+            }
+
+            if (currentKeyboard[Key.O] && !previousKeyboard[Key.O])
+            {
+                objy.ToggleVisibility();
+            }
+
+            // camera control (isometric mode)
+            if (currentKeyboard[Key.W])
+            {
+                cam.MoveForward();
+            }
+            if (currentKeyboard[Key.S])
+            {
+                cam.MoveBackward();
+            }
+            if (currentKeyboard[Key.A])
+            {
+                cam.MoveLeft();
+            }
+            if (currentKeyboard[Key.D])
+            {
+                cam.MoveRight();
+            }
+            if (currentKeyboard[Key.Q])
+            {
+                cam.MoveUp();
+            }
+            if (currentKeyboard[Key.E])
+            {
+                cam.MoveDown();
+            }
+            // camera rotation
+            if (currentKeyboard[Key.Comma])
+            {
+                cam.RotateLeft();
+            }
+            if (currentKeyboard[Key.Period])
+            {
+                cam.RotateRight();
+            }
+
+
+            // helper functions
+            if (currentKeyboard[Key.L] && !previousKeyboard[Key.L])
+            {
+                displayMarker = !displayMarker;
+            }
+
+            previousKeyboard = currentKeyboard;
+            previousMouse = currentMouse;
+            // END logic code
+            float deltaTime = (float)e.Time;
+
+            foreach (var cube in cubes)
+            {
+                cube.Update(deltaTime, 9.8f);
+            }
+        }
+
+        /// <summary>
+        /// OnRenderFrame() method. Part of the control loop of the OpenTK API. Executed periodically, with a frequency determined when launching
+        /// the graphics window (<see cref="GameWindow.Run(double, double)"/>). In this case should be 0.00 (if unmodified) - the rendering is triggered
+        /// only when the scene is modified.
+        ///
+        /// All render calls should reside here!
+        /// </summary>
+        /// <param name="e">event parameters that triggered the method;</param>
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            framesCounter++;
+
+            if (displayMarker)
+            {
+                TimeStampIt("render", framesCounter.ToString());
+            }
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            // RENDER CODE
+            grid.Draw();
+            ax.Draw();
+            objy.Draw();
+
+            foreach (var cube in cubes)
+            {
+                cube.Draw();
+            }
+
+            // END render code
+
+            SwapBuffers();
+        }
+
+        /// <summary>
+        /// Internal method, used to dump the menu on the console window (text mode!)...
+        /// </summary>
+        private void DisplayHelp()
+        {
+            Console.WriteLine("\n      MENIU");
+            Console.WriteLine(" (H) - meniul");
+            Console.WriteLine(" (ESC) - parasire aplicatie");
+            Console.WriteLine(" (K) - schimbare vizibilitate sistem de axe");
+            Console.WriteLine(" (R) - resteaza scena la valori implicite");
+            Console.WriteLine(" (B) - schimbare culoare de fundal");
+            Console.WriteLine(" (V) - schimbare vizibilitate linii");
+            Console.WriteLine(" (W,A,S,D) - deplasare camera (izometric)");
+            Console.WriteLine(" Click stanga - Spawneaza cub");
+            Console.WriteLine(" Click dreapta - Curata lista de cuburi");
+            Console.WriteLine(" (<,>) - rotatie camera");
+        }
+
+        private void TimeStampIt(String source, String counter)
+        {
+            String dt = DateTime.Now.ToString("hh:mm:ss.ffff");
+            Console.WriteLine("     TSTAMP from <" + source + "> on iteration <" + counter + ">: " + dt);
+        }
+
+    }
+
+
+    public class Cube
+    {
+        public Vector3 Position { get; private set; }
+        private float Size { get; set; }
+        private Color4 Color { get; set; }
+
+        public Cube(Vector3 position, float size, Color4 color)
+        {
+            Position = position;
+            Size = size;
+            Color = color;
+        }
+
+        public void Update(float deltaTime, float gravity)
+        {
+            if (Position.Y > 0)
+            {
+                Position -= new Vector3(0, deltaTime * gravity, 0);
+                if (Position.Y < 0)
+                {
+                    Position = new Vector3(Position.X, 0, Position.Z);
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            GL.PushMatrix();
+            GL.Translate(Position);
+            GL.Scale(Size, Size, Size);
+            GL.Color4(Color);
+
+            GL.Begin(PrimitiveType.Quads);
+            // Front face
+            GL.Vertex3(-0.5f, -0.5f, 0.5f);
+            GL.Vertex3(0.5f, -0.5f, 0.5f);
+            GL.Vertex3(0.5f, 0.5f, 0.5f);
+            GL.Vertex3(-0.5f, 0.5f, 0.5f);
+            // Back face
+            GL.Vertex3(-0.5f, -0.5f, -0.5f);
+            GL.Vertex3(-0.5f, 0.5f, -0.5f);
+            GL.Vertex3(0.5f, 0.5f, -0.5f);
+            GL.Vertex3(0.5f, -0.5f, -0.5f);
+            // Top face
+            GL.Vertex3(-0.5f, 0.5f, -0.5f);
+            GL.Vertex3(-0.5f, 0.5f, 0.5f);
+            GL.Vertex3(0.5f, 0.5f, 0.5f);
+            GL.Vertex3(0.5f, 0.5f, -0.5f);
+            // Bottom face
+            GL.Vertex3(-0.5f, -0.5f, -0.5f);
+            GL.Vertex3(0.5f, -0.5f, -0.5f);
+            GL.Vertex3(0.5f, -0.5f, 0.5f);
+            GL.Vertex3(-0.5f, -0.5f, 0.5f);
+            // Right face
+            GL.Vertex3(0.5f, -0.5f, -0.5f);
+            GL.Vertex3(0.5f, 0.5f, -0.5f);
+            GL.Vertex3(0.5f, 0.5f, 0.5f);
+            GL.Vertex3(0.5f, -0.5f, 0.5f);
+            // Left face
+            GL.Vertex3(-0.5f, -0.5f, 0.5f);
+            GL.Vertex3(-0.5f, 0.5f, 0.5f);
+            GL.Vertex3(-0.5f, 0.5f, -0.5f);
+            GL.Vertex3(-0.5f, -0.5f, -0.5f);
+            GL.End();
+
+            GL.PopMatrix();
+        }
+    }
+}
